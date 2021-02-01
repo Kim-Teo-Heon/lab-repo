b03f4975ca5578c617e3ac082395b87cb98fb604
Kim-Teo-Heon <xbox1720@naver.com>
2021-02-01 11:43:18 +0900

    공지 메뉴 삭제

--- models/create.js
+++ models/create.js
@@ -1,11 +1,11 @@
 var db=require('../db');
 
 exports.create_data= function(req){
-    let sql = 'INSERT INTO lab_intro(img,text,filename) VALUES(?,?,?)';
-    let post = req.body
-    let img= req.file.location;
-    let text = post.text;
-    let filename = req.file.originalname;
+  let sql = 'INSERT INTO lab_intro(img,text,filename) VALUES(?,?,?)';
+  let post = req.body
+  let img= req.file.location;
+  let text = post.text;
+  let filename = req.file.originalname;
 
     db.query(sql, [img,text,filename],function (err, data) {
     if(err) {
